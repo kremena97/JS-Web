f6689d501700d7eef575c9b8a4f0a736aed7882c
kremena97 <32265399+kremena97@users.noreply.github.com>
2019-10-11 19:09:02 +0300

    JS Back-End

---
+++ JS Back-End/Cat-Shelter-Workshop-NodeJS/resources/index.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const port = 3000;
+
+http.createServer((req,res)=>{
+    res.writeHead(200, {
+        'Content-Type': 'text/plain'
+    });
+
+    res.write('HELLO JS WORLD');
+
+    res.end();
+}).listen(port);
